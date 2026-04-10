0296952c9faca95f5f6f0c2a66ab96a3ab5dd268
Joery van Rooij <joery.van.rooij@swisssense.nl>
2026-04-10 12:55:47 +0200

    P2-2 adapter protocols: market and macro interfaces

---
+++ data/adapters/__init__.py
@@ -0,0 +1,5 @@
+"""Adapter protocols and implementations for the canonical data layer."""
+
+from data.adapters.base import MacroAdapter, MarketAdapter
+
+__all__ = ["MacroAdapter", "MarketAdapter"]
